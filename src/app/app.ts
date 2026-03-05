619c6a8ed2da1968ac7f9640163f440a1aa3f6a8
MAULIK PATEL <maulikspatel295@gmail.com>
2026-03-05 13:01:10 +0530

    First Change
    Interpolation using {{ propName }}

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,9 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('first-angular-21-app');
+  version = 21;
 }
