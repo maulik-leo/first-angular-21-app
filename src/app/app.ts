01e716a4e83aa385feea7c54aa0c9fe6e1357115
MAULIK PATEL <maulikspatel295@gmail.com>
2026-03-09 17:20:44 +0530

    Data Type

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,9 +9,20 @@ import { Custom } from './custom/cutom';
   styleUrl: './app.css',
 })
 export class App {
-  handleClick() {
-    console.log("Button Clicked");
-  }
   protected readonly title = signal('first-angular-21-app');
   version = 21;
+  author: string = 'Unknown';
+  age: string | number = 'NA';
+  comments: any = false;
+
+  handleClick() {
+    this.author = 'Maulik';
+    this.age = 27;
+
+    this.comments = 0;
+    this.comments = 'Modified Comments';
+    this.comments = { isAvailabled: true, description: 'Modified Comments' };
+
+    console.log('Button Clicked by ' + this.author + ' (' + this.age + ' years old)');
+  }
 }
